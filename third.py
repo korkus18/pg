5a9cac07bcd6fd21bc32afaf9738aa6c5212f97b
jakub.korous <kubak.ko@seznam.cz>
2024-10-19 15:36:06 +0200

    add third.py

--- third.py
+++ third.py
@@ -1,9 +1,5 @@
 def je_prvocislo(cislo):
-    """
-    Funkce overi, zda zadane cislo je nebo neni prvocislo a vrati True nebo False.
 
-    Prvocislo je takove cislo vetsi nez 1, ktere neni delitelne zadnym jinym cislem nez 1 a samo sebou.
-    """
     if cislo <= 1:
         return False
     for i in range(2, int(cislo ** 0.5) + 1):
@@ -13,9 +9,7 @@ def je_prvocislo(cislo):
 
 
 def vrat_prvocisla(maximum):
-    """
-    Funkce spocita vsechna prvocisla v rozsahu 1 az maximum a vrati je jako seznam.
-    """
+
     try:
         maximum = int(maximum)
     except ValueError:
